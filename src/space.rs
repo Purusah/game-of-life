5f9a74b81105adc25691f1c324337dc4a81b7de6
purusah <bohaichuk.v@gmail.com>
2022-09-13 00:02:29 +0300

    Almost infinite evolution

--- src/space.rs
+++ src/space.rs
@@ -13,12 +13,25 @@ impl Space {
         return Self { field: field };
     }
 
-    pub fn get_neighbor_at(&self, row: usize, column: usize) -> State {
+    pub fn get_neighbor_at(&self, row: i64, column: i64) -> State {
+        let size = self.field.len() as i64;
+
+        let row_wrapped = if row < 0 || row > size {
+            size % row
+        } else {
+            row
+        } as usize;
+        let column_wrapped = if column < 0 || column > size {
+            size % column
+        } else {
+            column
+        } as usize;
+
         if *self
             .field
-            .get(row)
+            .get(row_wrapped)
             .unwrap_or(&Vec::new())
-            .get(column)
+            .get(column_wrapped)
             .unwrap_or(&State::Dead)
             == State::Alive
         {
@@ -28,32 +41,24 @@ impl Space {
         }
     }
 
-    fn get_next_state(&self, row: usize, column: usize) -> State {
-        let current = *self.field.get(row).unwrap().get(column).unwrap();
+    fn get_next_state(&self, row_current: usize, column_current: usize) -> State {
+        let row = row_current as i64;
+        let column = column_current as i64;
+        let current = *self
+            .field
+            .get(row_current)
+            .unwrap()
+            .get(column_current)
+            .unwrap();
 
         let right = self.get_neighbor_at(row, column + 1) as usize;
         let down_right = self.get_neighbor_at(row + 1, column + 1) as usize;
         let down = self.get_neighbor_at(row + 1, column) as usize;
-
-        let mut down_left: usize = 0;
-        let mut left: usize = 0;
-        let mut up_left: usize = 0;
-        let mut up: usize = 0;
-        let mut up_right: usize = 0;
-
-        if column > 0 {
-            down_left = self.get_neighbor_at(row + 1, column - 1) as usize;
-            left = self.get_neighbor_at(row, column - 1) as usize;
-        }
-
-        if row > 0 {
-            up = self.get_neighbor_at(row - 1, column) as usize;
-            up_right = self.get_neighbor_at(row - 1, column + 1) as usize
-        }
-
-        if row > 0 && column > 0 {
-            up_left = self.get_neighbor_at(row - 1, column - 1) as usize;
-        }
+        let down_left = self.get_neighbor_at(row + 1, column - 1) as usize;
+        let left = self.get_neighbor_at(row, column - 1) as usize;
+        let up_left = self.get_neighbor_at(row - 1, column - 1) as usize;
+        let up = self.get_neighbor_at(row - 1, column) as usize;
+        let up_right = self.get_neighbor_at(row - 1, column + 1) as usize;
 
         let neighbors = up_left + up + up_right + right + down_right + down + down_left + left;
         if current == State::Alive {
